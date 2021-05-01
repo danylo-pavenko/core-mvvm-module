a433ac310d4646698c107681a24b72efc00d2cd8
Danylo Pavenko <daniil.pavenko@gmail.com>
2021-05-01 15:49:21 +0300

    Update deps add ext for nav types

--- core/src/main/java/com/dansdev/core/util/NavigationExt.kt
+++ core/src/main/java/com/dansdev/core/util/NavigationExt.kt
@@ -1,8 +1,17 @@
 package com.dansdev.core.util
 
+import android.os.Bundle
+import androidx.core.os.bundleOf
 import androidx.navigation.NavDirections
 import com.dansdev.core.model.NavigateDir
 
 fun NavDirections.asDir(): NavigateDir {
     return NavigateDir.XDir(this)
+}
+
+fun Int.asNavDirection(bundle: Bundle = bundleOf()): NavDirections = object : NavDirections {
+
+    override fun getActionId(): Int = this@asNavDirection
+
+    override fun getArguments(): Bundle = bundle
 }
